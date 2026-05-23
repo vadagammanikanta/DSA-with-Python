cdeb2e841ca0ebbf55b8cb92e28426bce8cb172e
vadagammanikanta <vadagammanikanta2006@gmail.com>
2026-05-23 15:53:28 +0530

    Started Python Basics for DSA

---
+++ Python Basics/Basics.py
@@ -0,0 +1,64 @@
+from asyncio.windows_events import NULL
+from operator import truediv
+
+print("Hello World")
+
+a=10
+b=10.9
+c=' Mani Kanta '
+d="Manikanta"
+words=["Mani","Kanta"]
+e=False
+f=[10,20,30,"Mani"]
+g=(10,20,30,"Mani")
+h={10,20,30,"Mani"}
+j={"name":"mani","age":20}
+
+
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(h)
+print(type(h))
+print(j)
+print(type(j))
+
+#String Functions
+print(d.lower())
+print(d.upper())
+print(c.strip())
+print(c.lstrip())
+print(c.rstrip())
+print(d.replace('a','x'))
+print(c.split())
+print(" ".join(words))
+print(c.find("a"))
+print(c.find("x"))
+print(d.index("M"))
+print(d.count("a"))
+print(d.startswith("Ma"))
+print(d.endswith("ta"))
+print(d.isalpha())
+print(d.isnumeric())
+print(d.isdigit())
+print(d.isalnum())
+print(d.isupper())
+print(d.islower())
+print(c.isspace())
+print(d.capitalize())
+print(c.title())
+print(d.swapcase())
+
+
